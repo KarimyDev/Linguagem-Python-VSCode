b658c2b08b590db4bdd9e0bf3f85bb88cf5947fe
Karimy <141510083+KarimyDev@users.noreply.github.com>
2026-01-12 12:17:15 -0300

    Create entrada_dados.py

---
+++ pycharm/entrada_dados.py
@@ -0,0 +1,15 @@
+nome1 = input("Nome da primeira pessoa: ")
+salario1 = float(input("Salario da primeira pessoa: "))
+
+nome2 = input("Nome da segunda pessoa: ")
+salario2 = float(input("Salario da segunda pessoa: "))
+
+idade = int(input("Digite uma idade: "))
+sexo = input("Digite um sexo (F/M): ")
+
+print(f"Primeiro nome: {nome1}")
+print(f"Primeiro salario: {salario1:.2f}")
+print(f"Segundo nome: {nome2}")
+print(f"Segundo salario: {salario2:.2f}")
+print(f"Idade: {idade}")
+print(f"Sexo: {sexo}")
